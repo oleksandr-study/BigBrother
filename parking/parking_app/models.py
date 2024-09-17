35ec8d336919ed8d902a38b8ba10d767f6d33cc2
Vrokmoit <vrokmoit.cattus@gmail.com>
2024-09-17 16:46:30 +0300

    Пофиксила экспорт и сделала корректный расчёт длительности парковки после добавления парковки через админку

--- parking/parking_app/models.py
+++ parking/parking_app/models.py
@@ -1,18 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from carplates.models import CarPlate
+from decimal import Decimal
 
 
 class Parking(models.Model):
     carplate = models.ForeignKey(CarPlate, on_delete=models.CASCADE)
     parked_at = models.DateTimeField(auto_now_add=True)
     unparked_at = models.DateTimeField(null=True)
-    parked_time = models.DurationField(null=True)
-    value = models.FloatField(default=0)
+    parked_time = models.DurationField(null=True, blank=True)
+    value = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'))
     # user = models.ForeignKey(CarUser, on_delete=models.CASCADE)
 
-    def set_parked_time(self):
-        self.parked_time = self.unparked_at - self.parked_at
+    def save(self, *args, **kwargs):
+        if self.parked_at and self.unparked_at:
+            self.parked_time = self.unparked_at - self.parked_at
+        super().save(*args, **kwargs)
+
+    def formatted_parked_time(self):
+        if self.parked_time is None:
+            return ''
+        total_seconds = self.parked_time.total_seconds()
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return '{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds))
 
     def __str__(self):
         return self.carplate.plate_number
--- parking/parking_app/urls.py
+++ parking/parking_app/urls.py
@@ -5,5 +5,5 @@ app_name='parking_app'
 urlpatterns = [
     path('<str:plate_number>', views.new_parking, name='new_parking'),
     path('export/csv', views.export_csv, name='export_csv'),
-    path('add-prices', views.create_prices, name='create_prices')
+    path('add/prices', views.create_prices, name='create_prices')
 ]
